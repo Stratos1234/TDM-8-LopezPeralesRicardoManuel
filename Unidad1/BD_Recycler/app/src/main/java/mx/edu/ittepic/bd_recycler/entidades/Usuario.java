2afa751b3810afe1546d70066279ef47b6d1e84e
unknown <rimalopezpe@ittepic.edu.mx>
2019-02-25 08:28:05 -0700

    Recycler base de datos

---
+++ Unidad1/BD_Recycler/app/src/main/java/mx/edu/ittepic/bd_recycler/entidades/Usuario.java
@@ -0,0 +1,41 @@
+package mx.edu.ittepic.bd_recycler.entidades;
+
+
+
+
+
+public class Usuario {
+    private int Clave;
+    private String nombre;
+    private double sueldo;
+
+    public Usuario(int clave, String nombre, double sueldo) {
+        Clave = clave;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+
+    public int getClave() {
+        return Clave;
+    }
+
+    public void setClave(int clave) {
+        Clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+}
